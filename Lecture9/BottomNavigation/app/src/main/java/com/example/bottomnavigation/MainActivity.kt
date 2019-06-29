17ab2479c482a40d093fe69f589f6343966b52b6
Ishaan Jain <31792878+IronClad1607@users.noreply.github.com>
2019-06-29 14:02:53 +0530

    Snackbar implemented

--- Lecture9/BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ Lecture9/BottomNavigation/app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -5,7 +5,9 @@ import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_blank.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onNavigationItemSelected(item: MenuItem): Boolean = when (item.itemId) {
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             true
         }
         R.id.navigation_notifications -> {
-            supportFragmentManager.beginTransaction().replace(R.id.container,BlankFragment()).commit()
+            Snackbar.make(container,"SnackBar Displayed",Snackbar.LENGTH_SHORT).show()
             true
         }
 
