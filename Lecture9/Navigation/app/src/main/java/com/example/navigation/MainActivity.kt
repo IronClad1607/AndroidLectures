3684de668efeecd353970adace31122f67bca809
Ishaan Jain <31792878+IronClad1607@users.noreply.github.com>
2019-06-29 12:57:33 +0530

    Added Menu Options

--- Lecture9/Navigation/app/src/main/java/com/example/navigation/MainActivity.kt
+++ Lecture9/Navigation/app/src/main/java/com/example/navigation/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.navigation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,24 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbaar)
         supportActionBar?.title = "Hello World"
+
+        btnChangeActivity.setOnClickListener {
+            startActivity(Intent(this@MainActivity, Main2Activity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.button1 -> {
+            Toast.makeText(this, "Toolbar Button", Toast.LENGTH_LONG).show()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+
     }
 }
