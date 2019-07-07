88ddcbbac3188ee18f570715a031faad7f9460f4
Ishaan Jain <31792878+IronClad1607@users.noreply.github.com>
2019-07-07 14:19:25 +0530

    Lecture 13: Hardware Sensors

--- Lecture13/FileReadAndWrite/app/src/main/java/com/example/filereadandwrite/MainActivity.kt
+++ Lecture13/FileReadAndWrite/app/src/main/java/com/example/filereadandwrite/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.filereadandwrite
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
+import java.io.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,21 +17,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ActivityCompat.requestPermissions(this,
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ),
+            111
+        )
 
-        val file = File(filesDir,"myfile.txt")
-        Log.d("FILES","Files dir = ${filesDir.path}")
+        Log.d("FILES", "Files dir = ${filesDir.path}")
+
+
+        val extDir = getExternalFilesDir(null)
+        Log.d("FILES", "External dir = ${extDir!!.path}")
+
+
+        val file = File(extDir, "myfile.txt")
+
+
 
         btnSave.setOnClickListener {
-            Log.d("FILES","exists = ${file.exists()}")
-            file.appendText(etData.text.toString())
+            // Save data of etData to file
+
+            Log.d("FILES", "exists = ${file.exists()}")
+//            file.writeText(etData.text.toString())
+            file.writeText(etData.text.toString())
+
+//            val foutStream = FileOutputStream(file)
+//            val bytes = etData.text.toString().toByteArray()
+//            foutStream.write(bytes)
+//            foutStream.close()
         }
 
         btnRestore.setOnClickListener {
+            // Restore data from file and show in tvFileText
+            val existingText = file.readText()
+            tvFileData.text = existingText
 
-
-            val existingData = file.readText()
-            tvFileData.text = existingData
-
+//            val fis = FileInputStream(file)
+//            val byteArray = ByteArray(20)
+//            val isr = InputStreamReader(fis)
+//            val br = BufferedReader(isr)
+//            br.readLine()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        if ((grantResults[0] == PackageManager.PERMISSION_DENIED) || (grantResults[1] == PackageManager.PERMISSION_DENIED)) {
+            Toast.makeText(
+                this,
+                "Cannot run without permissions",
+                Toast.LENGTH_SHORT
+            ).show()
+            finish()
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
