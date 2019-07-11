72f47d994f43a25c047c8ff8d7739fc09c779c26
Ishaan Jain <31792878+IronClad1607@users.noreply.github.com>
2019-07-11 13:54:37 +0530

    AppDatabase Created

--- Lecture14/RoomNotes/app/src/main/java/com/example/roomnotes/MainActivity.kt
+++ Lecture14/RoomNotes/app/src/main/java/com/example/roomnotes/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.roomnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val db = Room.databaseBuilder(
+            this,
+            AppDatabase::class.java,
+            "todo.db"
+        ).allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build()
     }
+
+
+
 }
