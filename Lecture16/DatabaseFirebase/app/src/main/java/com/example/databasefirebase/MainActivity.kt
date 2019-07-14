ed811f48db82c6389c7d357b3ae2ec53152a819b
Ishaan Jain <31792878+IronClad1607@users.noreply.github.com>
2019-07-14 13:55:50 +0530

    Firebase Database Implemented

--- Lecture16/DatabaseFirebase/app/src/main/java/com/example/databasefirebase/MainActivity.kt
+++ Lecture16/DatabaseFirebase/app/src/main/java/com/example/databasefirebase/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.databasefirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        FirebaseDatabase.getInstance().reference.child("messages")
+            .child("user1")
+            .child("chat")
+            .child("Time")
+            .addValueEventListener(object : ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {
+
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    text.text = p0.value.toString()
+                }
+
+            })
     }
 }
